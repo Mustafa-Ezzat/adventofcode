3c3eac1d99a969f6d9c2f09580cb51a09c341030
Mustafa <mustafa.ezzat@backstage.com>
2024-12-10 14:09:18 +0200

    day 09

--- 2024/D09/main.py
+++ 2024/D09/main.py
@@ -4,20 +4,20 @@ with open("testCases.txt", "r") as f:
 with open("input.txt", "r") as f:
     input = f.read().strip()
 
-def prepare_data():
+def init_disk():
     data = list(map(int, input))
     id = 0
-    block_list = []
+    disk = []
     for i in range(len(data)):
         item = data[i]
         if item == 0:
             continue
         if i % 2 == 0:
-            block_list += [str(id)] * item
+            disk += [str(id)] * item
             id += 1
         else:
-            block_list += ['.'] * item
-    return block_list
+            disk += ['.'] * item
+    return disk
         
 def check_sum(disk):
     sum = 0
@@ -36,46 +36,46 @@ def find_file_span(disk, file_id):
             end = i
             break
 
-    return start, end - 1
+    return start, end
 
-def find_leftmost_space(disk, size):
-    free_start = None
-    free_count = 0
+def find_left_most_space(disk, size):
+    first_dot = None
+    dots = 0
     for i, block in enumerate(disk):
         if block == '.':
-            if free_start is None:
-                free_start = i
-            free_count += 1
-            if free_count == size:
-                return free_start
+            if first_dot is None:
+                first_dot = i
+            dots += 1
+            if dots == size:
+                break
         else:
-            free_start = None
-            free_count = 0
-    return None
+            first_dot = None
+            dots = 0
+    return first_dot
 
-def move_file(disk, file_id, start, end, target_start):
-    size = end - start + 1
-    for i in range(start, end + 1):
+def relocate_file(disk, file_id, start, end, target_start):
+    size = end - start
+    for i in range(start, end):
         disk[i] = '.'
     for i in range(size):
         disk[target_start + i] = file_id
 
+def unique_file_ids(input_list):
+    return list(map(str, sorted(list(map(int, set([item for item in input_list if item != '.']))), reverse=True)))
+
 def relocate_files(disk):
-    file_ids = get_file_ids(disk)
+    file_ids = unique_file_ids(disk)
     for file_id in file_ids:
         start, end = find_file_span(disk, file_id)
-        size = end - start + 1
-        target_start = find_leftmost_space(disk, size)
+        size = end - start
+        target_start = find_left_most_space(disk, size)
         if target_start is not None and target_start < start:
-            move_file(disk, file_id, start, end, target_start)
+            relocate_file(disk, file_id, start, end, target_start)
 
     return disk
 
-def get_file_ids(input_list):
-    return list(map(str, sorted(list(map(int, set([item for item in input_list if item != '.']))), reverse=True)))
-
 def solve_part_one():
-    disk = prepare_data()
+    disk = init_disk()
     i, j = 1, len(disk) - 1
     while j > i:
         if disk[i] != '.':
@@ -89,7 +89,7 @@ def solve_part_one():
     return check_sum(disk[:i+1])
 
 def solve_part_two():
-    disk = prepare_data()
+    disk = init_disk()
     disk = relocate_files(disk)
     return check_sum(disk)
 
